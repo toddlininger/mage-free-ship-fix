130c260fd8b810c24f68806d8ac9f24e8c6ae536
Todd Lininger <design@toddlininger.com>
2015-10-29 15:49:20 -0700

    Made code upgrade-proof thanks to @nikhilben

--- app/code/community/ToddLininger/FreeShipping/Model/Carrier/Freeshipping.php
+++ app/code/community/ToddLininger/FreeShipping/Model/Carrier/Freeshipping.php
@@ -4,46 +4,26 @@
  * @description  Simple extension to fix bug where free shipping is still available when discount brings total under free shipping threshold
  * @author       Todd Lininger, https://toddlininger.com
  * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @thanks       Thanks to Nikhil Ben Kuruvilla for superior upgrade-proof code: https://github.com/nikhilben/magento-free-shipping-and-discount-coupon
  */
 class ToddLininger_FreeShipping_Model_Carrier_Freeshipping extends Mage_Shipping_Model_Carrier_Freeshipping
 {
-    /**
-     * FreeShipping Rates Collector
-     *
-     * @param Mage_Shipping_Model_Rate_Request $request
-     * @return Mage_Shipping_Model_Rate_Result
-     */
-    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
-    {
-        if (!$this->getConfigFlag('active')) {
-            return false;
-        }
-
-        $result = Mage::getModel('shipping/rate_result');
-
-        $this->_updateFreeMethodQuote($request);
-
-		if (($request->getFreeShipping())
-			// BEGIN CUSTOM CODE
-            // || ($request->getBaseSubtotalInclTax() >= $this->getConfigData('free_shipping_subtotal'))
-			|| ($request->getPackageValueWithDiscount() >= $this->getConfigData('free_shipping_subtotal'))
-			// END CUSTOM CODE
-        ) {
-            $method = Mage::getModel('shipping/rate_result_method');
-
-            $method->setCarrier('freeshipping');
-            $method->setCarrierTitle($this->getConfigData('title'));
-
-            $method->setMethod('freeshipping');
-            $method->setMethodTitle($this->getConfigData('name'));
-
-            $method->setPrice('0.00');
-            $method->setCost('0.00');
-
-            $result->append($method);
-        }
-
-        return $result;
-    }
+	/**
+	 * The original free shipping class will use the discounted package value.
+	 *
+	 * The package_value_with_discount value already is in the base currency
+	 * even if there is no "base" in the property name, no need to convert it.
+	 *
+	 * @param Mage_Shipping_Model_Rate_Request $request
+	 * @return Mage_Shipping_Model_Rate_Result
+	 */
+	public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+	{
+		$baseSubtotal = $request->getBaseSubtotalInclTax();
+		$request->setBaseSubtotalInclTax($request->getPackageValueWithDiscount());
+		$result = parent::collectRates($request);
+		$request->setBaseSubtotalInclTax($baseSubtotal);
+		return $result;
+	}
 
 }
